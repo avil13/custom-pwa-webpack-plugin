7597e4ed13d3155d80e2f3ab353f20328e7b803b
avil13 <13.avil@gmail.com>
2018-08-03 12:18:25 +0300

    add loaders

---
+++ src/lib/webpack_child_process.js
@@ -0,0 +1,38 @@
+// @ts-check
+const webpack = require('webpack');
+const path = require('path');
+
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, '..', 'dist'),
+        filename: 'service-worker.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.ts', '.json']
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        overlay: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
+};
